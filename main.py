26ae28328b94c2aa81276ce279a24025d518192b
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-05-10 22:42:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -77,45 +77,41 @@ if page == "Chiffrement de texte":
                 st.error("Une erreur est survenue lors du déchiffrement. Vérifiez la clé et les informations du message.")
 else:
     st.header("Chiffrement et déchiffrement de fichiers")
-    key_input = st.text_input("Entrer votre clé de chiffrement")
-    
+  key_input = st.text_input("Entrer votre clé de chiffrement")
 
+if key_input:
+    key = get_key(key_input)
+else:
+    if st.button("Générer une clé"):
+        key = get_random_bytes(32)
+        key_str = b64encode(key).decode()
+        st.success(f"Clé générée (Gardez-la précieusement, nous ne la sauvegarderons pas !): {key_str}")
 
-    if key_input:
-        key_ = get_key(key_input)
-    else :
-        if st.button("Générer une clé"):
-            key1 = get_random_bytes(32)
-            key2 = hashlib.sha256(key_.encode()).digest()
-            st.success(f"Clé générée (Gardée la précieusement nous ne la sauvegarderons pas ! ): {b64encode(key1).decode()}")
+uploaded_file = st.file_uploader("Choisissez un fichier à chiffrer", type=["png", "jpg", "txt", "pdf", "enc"])
 
-    uploaded_file = st.file_uploader("Choisissez un fichier à chiffrer", type=["png", "jpg", "txt", "pdf", "enc"])
+if st.button("Chiffrer"):
+    if uploaded_file and key:
+        file_bytes = uploaded_file.read()
+        file_to_encrypt = io.BytesIO(file_bytes)
 
-    if st.button("Chiffrer"):
-        if uploaded_file and key_:
-            file_bytes = uploaded_file.read()
-            file_to_encrypt = io.BytesIO(file_bytes)
+        encrypted_file_info = encrypt_file(file_to_encrypt, key)
+        st.success("Fichier chiffré !")
 
-            encrypted_file_info = encrypt_file(file_to_encrypt, key)
-            st.success("Fichier chiffré !")
+        # Convert encrypted file info to string and encode it to bytes
+        encrypted_file_info_bytes = json.dumps(encrypted_file_info).encode()
 
-            # Convert encrypted file info to string and encode it to bytes
-            encrypted_file_info_bytes = json.dumps(encrypted_file_info).encode()
+        # Offer the encrypted file info for download
+        st.download_button(
+            label="Télécharger le fichier chiffré",
+            data=encrypted_file_info_bytes,
+            file_name='encrypted_file.enc',
+            mime='text/plain'
+        )
 
-            # Offer the encrypted file info for download
-            st.download_button(
-                label="Télécharger le fichier chiffré",
-                data=encrypted_file_info_bytes,
-                file_name='encrypted_file.enc',
-                mime='text/plain'
-            )
-
-    if st.button("Déchiffrer"):
-        
-            st.write(key_)
-    
-       # try:
-            decrypted_data, file_extension = decrypt_file(uploaded_file.read(), key_)
+if st.button("Déchiffrer"):
+    if uploaded_file and key:
+        try:
+            decrypted_data, file_extension = decrypt_file(uploaded_file.read(), key)
             decrypted_file = io.BytesIO(decrypted_data)
             st.download_button(
                 label="Télécharger le fichier déchiffré",
@@ -123,5 +119,5 @@ else:
                 file_name=f'decrypted_file{file_extension}',
                 mime='application/octet-stream'
             )
-        #except:
+        except:
             st.error("Une erreur est survenue lors du déchiffrement. Vérifiez la clé et les informations du fichier.")
